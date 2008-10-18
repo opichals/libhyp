a2d25dcfd614906531e8b81726ac407f5eec0feb
opichals@seznam.cz <opichals@seznam.cz@c51f240c-9d46-11dd-a29e-e3fa11317253>
2008-10-18 21:54:28 +0000

    Making the 'wget' command configurable (OS X has curl instead by default).

--- tools/hypviewcgi/config.pl
+++ tools/hypviewcgi/config.pl
@@ -5,7 +5,9 @@
 	cache => "cache",
 	tmp => "cache/extract",
 	href_image => "/image",
-	href_openhyp => "/"
+	href_openhyp => "/",
+	fetch => 'curl -qfL -o $FILE --url $url 2>/dev/null'
+#	fetch => 'wget -q -O $FILE -c $url 2>/dev/null'
 #	enca => "./enca/bin/enca -Lcs -x"
 );
 
